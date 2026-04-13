cbdd16645618e0ed946309b3dd7019f9890a2135
wlsgusor53-png <wlsgusor53@gmail.com>
2026-04-13 14:34:57 +0900

    Update analyze.js

--- api/analyze.js
+++ api/analyze.js
@@ -16,6 +16,7 @@ export default async function handler(req, res) {
       },
       body: JSON.stringify(req.body)
     });
+
     const data = await response.json();
     return res.status(200).json(data);
   } catch (err) {
